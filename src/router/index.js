983da266b275a8daa21a81c61ba485a6416dc6ef
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-12-19 13:23:34 +0100

    добавил страницы для промо

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RussianView from '../views/RussianView.vue'
+import EnglishView from '../views/EnglishView.vue'
+import TurkishView from '../views/TurkishView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: HomeView,
+		},
+		{
+			path: '/ru',
+			name: 'RussianView',
+			component: RussianView,
+		},
+		{
+			path: '/en',
+			name: 'EnglishView',
+			component: EnglishView,
+		},
+		{
+			path: '/tr',
+			name: 'TurkishView',
+			component: TurkishView,
+		},
+	],
 })
 
 export default router
