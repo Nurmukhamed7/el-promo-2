3b3e4d842c264d7efee2e44d71b9bb9da440482e
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-12-20 17:26:27 +0100

    секцию 3 добавил в 1

--- src/views/RussianView.vue
+++ src/views/RussianView.vue
@@ -1,7 +1,6 @@
 <script setup>
 import TheNavbar from '@/components/TheNavbar.vue'
 import SectionTwo from '@/components/SectionTwo.vue'
-import SectionThree from '@/components/SectionThree.vue'
 </script>
 
 <template>
@@ -112,7 +111,80 @@ import SectionThree from '@/components/SectionThree.vue'
 				</div>
 			</header>
 			<SectionTwo></SectionTwo>
-			<SectionThree></SectionThree>
+
+			<section class="wrapper">
+				<div class="logo rel">
+					<img src="@/assets/images/logo/logo.svg" alt="logo" />
+					<span>EasyLife</span>
+				</div>
+				<div class="how-header">
+					<h3>{{ $t('How will the raffle results be summarized?') }}</h3>
+				</div>
+				<div class="how-paragraph">
+					<p>
+						{{ $t('Every Sunday') }}
+					</p>
+					<p>
+						{{ $t('The Winner') }}
+					</p>
+				</div>
+				<div class="bg">
+					<img class="bg-image" src="@/assets/images/background15.png" alt="" />
+					<h1 class="luck">{{ $t('Good luck!') }}</h1>
+					<div class="instagram">
+						<img src="@/assets/images/instagram.svg" alt="instagram" />
+						<a href="https://www.instagram.com/easylife_turkiye" target="_blank"
+							>easylife_turkiye</a
+						>
+					</div>
+				</div>
+				<div class="downloads">
+					<a
+						v-if="$i18n.locale === 'ru'"
+						href="https://apps.apple.com/kz/app/easylife-turkiye/id1671635384?platform=iphone"
+						target="_blank"
+					>
+						<img
+							class="btn"
+							src="@/assets/images/appledownloadRu.svg"
+							alt="appledownloadRu"
+						/>
+					</a>
+					<a
+						v-else
+						href="https://apps.apple.com/kz/app/easylife-turkiye/id1671635384?platform=iphone"
+						target="_blank"
+					>
+						<img
+							class="btn"
+							src="@/assets/images/appledownloadEn.svg"
+							alt="appledownloadEn"
+						/>
+					</a>
+					<a
+						v-if="$i18n.locale === 'ru'"
+						href="https://play.google.com/store/apps/details?id=com.easy_life.easy_life&hl=en_US"
+						target="_blank"
+					>
+						<img
+							class="btn"
+							src="@/assets/images/googledownloadRu.svg"
+							alt="googledownloadRu"
+						/>
+					</a>
+					<a
+						v-else
+						href="https://play.google.com/store/apps/details?id=com.easy_life.easy_life&hl=en_US"
+						target="_blank"
+					>
+						<img
+							class="btn"
+							src="@/assets/images/googledownloadEn.svg"
+							alt="googledownloadEn"
+						/>
+					</a>
+				</div>
+			</section>
 		</div>
 	</div>
 </template>
@@ -179,6 +251,57 @@ header {
 	left: 50%;
 	transform: translate(-50%, 12%);
 }
+.downloads {
+	margin-top: 22px;
+	display: flex;
+	justify-content: space-between;
+	.btn {
+		width: 108px;
+	}
+}
+
+.how-header {
+	margin: 31px 0 71px;
+	h3 {
+		color: $yellow; // yellow
+		text-align: center;
+		font-size: 24px;
+	}
+}
+
+.how-paragraph {
+}
+
+.bg {
+	position: relative;
+}
+
+.bg-image {
+	position: absolute;
+	top: -100px;
+	z-index: -10;
+	width: 300px; // 375px
+}
+
+.luck {
+	margin: 50px;
+	font-size: 48px;
+	text-align: center;
+}
+
+.instagram {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 13px;
+	a {
+		font-size: 20px;
+		font-weight: 800;
+		color: white;
+		text-decoration: underline;
+	}
+}
+
 .downloads {
 	margin-top: 22px;
 	display: flex;
